a5817ce8a749a4e7df5e5f12f84fde46bbcaa88c
Eito Takase <eito01178@gmail.com>
2023-02-11 13:00:43 +0900

    エラー時に通知を出す

--- src/extension/index.js
+++ src/extension/index.js
@@ -1,58 +1,75 @@
 
-let recentWindowName = ''
-
 function check() {
     fetch('http://localhost:9281/').then(async (res) => {
-        const json = await res.json()
-        const windowName = json.window_name
-        if (!windowName) {
-            console.log('no window name')
-            return
-        }
-        if (windowName === recentWindowName) {
-            return
-        }
-        recentWindowName = windowName
-        chrome.tabGroups.query(
-            {},
-            (groups) => {
-                console.log('callback', groups)
-                const group = groups.find(group => group.title === windowName)
-                if (!group) {
-                    console.log('no group')
-                    return
-                }
-                groups.forEach(group => {
-                    chrome.tabGroups.update(
-                        group.id,
-                        {
-                            collapsed: true
-                        }
-                    )
-                });
-                chrome.tabGroups.move(group.id, { index: 1 })
-                chrome.tabs.query(
-                    {
-                        groupId: group.id
-                    },
-                    (tabs) => {
-                        chrome.tabs.update(
-                            tabs[0].id,
-                            {
-                                active: true
-                            }
-                        )
-                    }
-                )
-            }
-        )
+        changeTabGroup(res)
+    }).catch(e => {
+        createNotification('通信エラー', e.toString())
     })
 }
 
-check()
+let recentWindowName = ''
 
-chrome.windows.onFocusChanged.addListener(
-    () => {
-        check()
+async function changeTabGroup (res) {
+    const json = await res.json()
+    const windowName = json.window_name
+    if (!windowName) {
+        createNotification('エラー', 'アクティブなtmuxセッションまたはウィンドウが見つかりません')
+        return
     }
-  )
+    if (windowName === recentWindowName) {
+        return
+    }
+    recentWindowName = windowName
+    chrome.tabGroups.query(
+        {},
+        (groups) => {
+            const group = groups.find(group => group.title === windowName)
+            if (!group) {
+                createNotification('エラー', 'ウィンドウ名と一致するタブグループが見つかりません')
+                return
+            }
+            groups.forEach(group => {
+                chrome.tabGroups.update(
+                    group.id,
+                    {
+                        collapsed: true
+                    }
+                )
+            });
+            chrome.tabGroups.move(group.id, { index: 1 })
+            chrome.tabs.query(
+                {
+                    groupId: group.id
+                },
+                (tabs) => {
+                    chrome.tabs.update(
+                        tabs[0].id,
+                        {
+                            active: true
+                        }
+                    )
+                }
+            )
+        }
+    )
+}
+
+/**
+ * 通知を表示する
+ * @param {string} title 
+ * @param {string} message 
+ */
+function createNotification (title, message) {
+    chrome.notifications.create({
+        type: 'basic',
+        iconUrl: '/icon.png',
+        title: title,
+        message: message,
+        priority: 1
+    });
+}
+
+check()
+chrome.windows.onFocusChanged.addListener(
+    () => check()
+)
