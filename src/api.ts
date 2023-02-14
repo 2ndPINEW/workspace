e770eee2b53957232884d46fec233de2e4326dc5
Eito Takase <eito01178@gmail.com>
2023-02-14 19:57:12 +0900

    format

--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,19 @@
-import { Application, Router, RouterContext } from "https://deno.land/x/oak@v6.5.0/mod.ts";
+import {
+  Application,
+  Router,
+  RouterContext,
+} from "https://deno.land/x/oak@v6.5.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import fallback from './fallback.json' assert { type: "json" }
+import fallback from "./fallback.json" assert { type: "json" };
 
 const app = new Application();
 const router = new Router();
 
 app.addEventListener("listen", ({ hostname, port, secure }) => {
   console.log(
-    `Listening on: ${secure ? "https://" : "http://"}${hostname ??
-      "localhost"}:${port}`,
+    `Listening on: ${secure ? "https://" : "http://"}${
+      hostname ?? "localhost"
+    }:${port}`
   );
 });
 
@@ -16,32 +21,35 @@ app.addEventListener("error", (evt) => {
   console.log(evt.error);
 });
 
-router.get('/active', async (ctx: RouterContext) => {
-  const p = Deno.run({ cmd: ["tmux", "lsw"], stdout: 'piped' });
-  await p.status()
-  const stdout = new TextDecoder().decode(await p.output())
-  const lines = stdout.split('\n')
+router.get("/active", async (ctx: RouterContext) => {
+  const p = Deno.run({ cmd: ["tmux", "lsw"], stdout: "piped" });
+  await p.status();
+  const stdout = new TextDecoder().decode(await p.output());
+  const lines = stdout.split("\n");
   const activeLine = lines.find((line) => line.includes("(active)"));
-  const windowNameMatcher = /(?<index>[0-9]{1}): (?<name>[\w/:%#\$&\?~\.=\+\-]+)(?<current>\*?) \((?<panel>[0-9]{1}) panes\)/;
-  const { name } = activeLine?.match(windowNameMatcher)?.groups as { name: string };
+  const windowNameMatcher =
+    /(?<index>[0-9]{1}): (?<name>[\w/:%#\$&\?~\.=\+\-]+)(?<current>\*?) \((?<panel>[0-9]{1}) panes\)/;
+  const { name } = activeLine?.match(windowNameMatcher)?.groups as {
+    name: string;
+  };
   ctx.response.body = {
     window_name: name,
-  }
-})
+  };
+});
 
-router.get('/fallback', (ctx: RouterContext) => {
-  ctx.response.body = fallback
-})
+router.get("/fallback", (ctx: RouterContext) => {
+  ctx.response.body = fallback;
+});
 
 router.post("/switch", async (ctx: RouterContext) => {
-  const body = await ctx.request.body()
-  const json = await body.value
-  const windowName = json.window_name
+  const body = await ctx.request.body();
+  const json = await body.value;
+  const windowName = json.window_name;
   if (windowName) {
     Deno.run({ cmd: ["tmux", "selectw", "-t", windowName] });
   }
-  ctx.response.body = ''
-})
+  ctx.response.body = "";
+});
 
 app.use(oakCors());
 app.use(router.routes());
