4e67c849c3def48d97e1a5d7a41661f60449d245
takase <takase@jig.jp>
2023-02-14 14:35:24 +0900

    chromeのタブ切り替え時にtmuxのウィンドウも切り替える

--- src/api.ts
+++ src/api.ts
@@ -33,6 +33,16 @@ router.get('/fallback', (ctx: RouterContext) => {
   ctx.response.body = fallback
 })
 
+router.post("/switch", async (ctx: RouterContext) => {
+  const body = await ctx.request.body()
+  const json = await body.value
+  const windowName = json.window_name
+  if (windowName) {
+    Deno.run({ cmd: ["tmux", "selectw", "-t", windowName] });
+  }
+  ctx.response.body = ''
+})
+
 app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
