55ac28605a5c6ff53d167aec9a2e002aa46cb948
Eito Takase <eito01178@gmail.com>
2023-02-24 01:13:30 +0900

    最初にワークスペース作る時の一覧を別ファイルにする

---
+++ tmux_layout/workspace.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+# セッションの名前
+SESSION_NAME=personal
+
+source tmux_layout/workspace.txt
+
+# 同じレイアウトで複数のワークスペースを作成
+# 同じレイアウトで複数のワークスペースを作成
+for ((I=1; I<=$WORKSPACE_COUNT; I++)); do
+  if [ $I = 1 ]; then
+    # 1つ目のワークスペースはセッション作成と同時に作成
+    tmux new-session -d -A -s "$SESSION_NAME" -c $WORKSPACE_PATH[$I] -x 64 -y 64
+  else
+    # 以降のワークスペースはウィンドウを作成
+    tmux new-window -c $WORKSPACE_PATH[$I]
+  fi
+
+  if [ $WORKSPACE_NAME[$I] = 'workspace' ]; then
+    tmux send-keys 'deno task serve' Enter
+  fi
+  
+  # ウィンドウの名前を変更
+  tmux rename-window "$WORKSPACE_NAME[$I]"
+
+  zsh tmux_layout/make_layout.sh
+done
+# 最初のウィンドウにフォーカスする
+tmux select-window -t 0
+
+# 現在のターミナルをセッションにアタッチする
+tmux attach -t $SESSION_NAME
