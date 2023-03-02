5525b0f71eadf2459031b9205aea2cc215bee136
Eito Takase <eito01178@gmail.com>
2023-03-03 00:33:08 +0900

    VSCodeからワークスペースの切り替えができるように

--- vscode-workspace-manager/src/extension.ts
+++ vscode-workspace-manager/src/extension.ts
@@ -1,31 +1,92 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import axios from 'axios';
+import { createWorkspaceWindow, fetchActiveWorkspace, fetchWorkspaces, switchWorkspaceWindow, Workspace } from "./util";
 
-const API_BASE = "http://localhost:9281/";
+export function activate(context: vscode.ExtensionContext) {
+  const disposables = [];
 
-export function activate() {
+  disposables.push(
+    vscode.commands.registerCommand(
+      "vscodeWorkspaceManager.openWorkspace",
+      () => openWorkspacePrompt()
+    )
+  );
+
+  context.subscriptions.push(...disposables);
+
   setInterval(() => {
-		checkActiveWorkspace();
+    checkActiveWorkspace();
   }, 1000);
 
-	checkActiveWorkspace();
+  checkActiveWorkspace();
 }
 
 export function deactivate() {}
 
-async function checkActiveWorkspace () {
-	const res = await axios.get(`${API_BASE}workspaces/active`);
-	const json = await res.data;
-	const targetWorkspace = json.workspace;
-	const currentWokspaceName = vscode.workspace.name;
-	if (targetWorkspace.name !== currentWokspaceName) {
-		openWorkspace(targetWorkspace.path);
-	}
+async function openWorkspacePrompt() {
+  const currentWokspaceName = vscode.workspace.name;
+  const workspaceEntries = (await fetchWorkspaces()).filter(entry => entry.name !== currentWokspaceName);
+  
+  if (!workspaceEntries.length || workspaceEntries.length <= 0) {
+    vscode.window.showInformationMessage("No workspaces found");
+    return;
+  }
+
+  const workspaceItems = workspaceEntries.map(
+    (entry) =>
+      <vscode.QuickPickItem>{
+        label: entry.name,
+        description: entry.hasTmuxWindow ? '' : '・',
+        detail: entry.path
+      }
+  );
+
+  const options = <vscode.QuickPickOptions>{
+    matchOnDescription: false,
+    matchOnDetail: false,
+    placeHolder: `Choose a workspace`,
+  };
+
+  vscode.window.showQuickPick(workspaceItems, options).then(
+    (workspaceItem: vscode.QuickPickItem | undefined) => {
+      if (!workspaceItem) {
+        return;
+      }
+
+      const entry = workspaceEntries.find(
+        (entry) => entry.path === workspaceItem.detail
+      );
+
+      if (!entry) {
+        return;
+      }
+
+      switchWorkspace(entry);
+    },
+    (reason: any) => {}
+  );
+}
+
+async function checkActiveWorkspace() {
+  const targetWorkspace = await fetchActiveWorkspace();
+  const currentWokspaceName = vscode.workspace.name;
+  if (targetWorkspace.name !== currentWokspaceName) {
+    openWorkspace(targetWorkspace.path);
+  }
 }
 
 function openWorkspace(path: string) {
   const workspaceUri = vscode.Uri.file(path);
-  vscode.commands.executeCommand("vscode.openFolder", workspaceUri, false);
+  vscode.commands.executeCommand("vscode.openFolder", workspaceUri, false).then(
+    () => {},
+    () => {}
+  );
+}
+
+async function switchWorkspace(entry: Workspace) {
+  if (entry.hasTmuxWindow) {
+    await switchWorkspaceWindow(entry.name);
+  } else {
+    await createWorkspaceWindow(entry.name);
+  }
+  await checkActiveWorkspace();
 }
---
+++ vscode-workspace-manager/src/util.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const API_BASE = "http://localhost:9281/";
+
+export interface Workspace {
+	name: string
+	path: string
+	hasTmuxWindow: boolean
+	isTmuxWindowActive: boolean
+}
+
+export async function fetchWorkspaces(): Promise<Workspace[]> {
+  const res = await axios.get(`${API_BASE}workspaces`);
+  return await res.data.workspaces;
+}
+
+export async function fetchActiveWorkspace(): Promise<Workspace> {
+  const res = await axios.get(`${API_BASE}workspaces/active`);
+  return await res.data.workspace;
+}
+
+export async function createWorkspaceWindow(name: string): Promise<void> {
+  await axios.get(`${API_BASE}tmux/windows/${name}/create`);
+}
+
+export async function switchWorkspaceWindow(name: string): Promise<void> {
+  await axios.get(`${API_BASE}tmux/windows/${name}/switch`);
+}
